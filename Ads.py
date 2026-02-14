7d288854889208006cb8288947e56d894af73390
sagoremulator-cmd <sagoremulator@gmail.com>
2026-02-14 07:28:28 +0600

    Update Ads.py

--- Ads.py
+++ Ads.py
@@ -14,13 +14,28 @@ ADS = [
     },
     {
         "headline": "*#Ads Inside Ads Monetize Telegram Channel*",
-        "body": "Highest Paying Monetization Service In Telegram",
+        "body": "High Paying Monetization Service In Telegram",
         "cta": {"label": "Monetize Now", "url": "https://t.me/InsideAds_bot/open?startapp=r_5083713667"}
     },
     {
-        "headline": "*#Ads Bitget Official *",
+        "headline": "*#Ads Bitget Official*",
         "body": "💰Get up to 500USDT welcome pack on your first launch and Enjoy 50% off transaction fees!",
         "cta": {"label": "Join Now", "url": "https://t.me/BitgetOfficialBot/Bitget?startapp=JwnaGhlngUX3oFNY1AUuJHFa38jeKvF"}
+    },
+    {
+        "headline": "*#Ads FoxiGrow *",
+        "body": "Earn rewards by completing tasks, Minimum Withdrawal 2USDT",
+        "cta": {"label": "Earn Now", "url": "https://t.me/FoxiGrowbot?start=ref_5083713667"}
+    },
+    {
+        "headline": "*#Ads Hot Wallet*",
+        "body": "Mine HOT On Near Protocol",
+        "cta": {"label": "Mine Now", "url": "https://app.hot-labs.org/link?7814048-village-279238"}
+    },
+    {
+        "headline": "*#Ads Gmail Farmer*",
+        "body": "Create Gmail Account And Get Paid",
+        "cta": {"label": "Join Now", "url": "https://t.me/GmailFarmerBot?start=5083713667"}
     }
 ]
 
@@ -29,13 +44,13 @@ def get_ads_keyboard(ad):
     return InlineKeyboardMarkup([[InlineKeyboardButton(ad["cta"]["label"], url=ad["cta"]["url"])]])
 
 async def maybe_show_ads(update):
-    """Show ads only if 6 hours passed since last ad for this user"""
+    """Show ads only if 1 hour passed since last ad for this user"""
     user_id = update.effective_user.id
     now = time.time()
     last_time = LAST_AD_TIME.get(user_id, 0)
 
-    # 6 hours = 21600 seconds
-    if now - last_time >= 21600:
+    # 1 hour = 3600 seconds
+    if now - last_time >= 3600:
         ad = random.choice(ADS)  # pick random ad
         await update.message.reply_text(
             f"{ad['headline']}\n{ad['body']}",
